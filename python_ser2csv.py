a3fbdaa7f946284902f03c592c917e4fa3cf92ef
Mousa Khan <mousakhan95@gmail.com>
2017-12-22 09:57:47 -0500

    Update python_ser2csv.py

--- python_ser2csv.py
+++ python_ser2csv.py
@@ -25,14 +25,11 @@ if len(arduino_ports) > 1:
 
 ser = serial.Serial(arduino_ports[0])
 
-# Get name and create a CSV file with that name
+
 name = raw_input('Name of session: ')
 file = open(name + '.csv', 'a')
-# Headers for the CSV file
-csv.writer(file).writerow(["Load 1 (lb)", "Load 2 (lb)", "Load 3 (lb)", "Total (lb)"])
 
-# Set up plot
-plt.ion()
+plt.ion() ## Note this correction
 fig=plt.figure()
 plt.xlabel('Time (s) ', fontsize=16)
 plt.ylabel('Weight (lb)', fontsize=16)
@@ -44,26 +41,26 @@ y=list()
 
 print("connected to: " + ser.portstr)
 print('Begin data collection for ' + name + '\n')
-
+# Headers for the CSV file
+csv.writer(file).writerow(["Load 1 (lb)", "Load 2 (lb)", "Load 3 (lb)", "Total (lb)"])
 while True:
 	line = ser.readline()
-    
-    # Get the values of each load and the total value
 	values = line.split(",")
-    
-	# Make sure all three values and the total value are present
+	# Check if there are 4 outputs 
 	if len(values) == 4:
 		totalValue = line.split(",")[3]
+		print(totalValue)
 		x.append(i);
 		y.append(totalValue);
-        # Reference line for when Servo activates
-		plt.axhline(35, color='g')
+		plt.axhline(40, color='g')
 		plt.plot(x, y, 'r');
 		plt.show();
-        # Pause is needed so it live updates properly
 		plt.pause(0.0001)
 		file.write(line + '\n')
-		i+=1;
+		i+=0.2;
+		if len(x) >= 11:
+			x.pop(0);
+			y.pop(0);
 	time.sleep(0.1)
 	
 
